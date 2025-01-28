95f70b566bbc1a6edc726d87cd85101b511d7158
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2025-01-28 14:12:34 +0100

    `Refactored All_Events component layout and styling`

--- src/components/sections/Home/All_Events/index.tsx
+++ src/components/sections/Home/All_Events/index.tsx
@@ -16,64 +16,68 @@ const All_Events: React.FC<{ events: IPost[]; donations: DonationsType[] }> = ({
           Upcoming Events
         </h2>
         <div className="w-16 h-1 bg-[var(--greencal-primary)]"></div>
-        <div className="mt-10 grid mb-20 gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {donations.map((x, i) => (
-            <div
-              key={i}
-              className="overflow-hidden max-w-sm bg-white rounded-xl shadow"
-            >
-              <div className="relative">
-                <div className="absolute top-3 right-3 py-2 px-3 text-sm rounded-lg bg-[var(--greencal-primary)] text-white">
-                  {x.category}
-                </div>
-                <div className="absolute bottom-0 left-3 py-1 px-3 text-sm rounded-tl-lg rounded-tr-lg bg-[var(--greencal-main)] text-white">
-                  {x.date}
-                </div>
-                <Image
-                  alt="Greencal upcoming event event"
-                  src={x.image}
-                  width={300}
-                  height={200}
-                  className="object-cover w-full h-auto"
-                />
-              </div>
-
-              <div className="p-4">
-                <div className="mb-2 flex gap-4 text-xs">
-                  <div className="flex items-center gap-2">
-                    <Icon name="bx-time" color="#B6E4CE" size="15px" />
-                    <span>{x.time}</span>
+        <div className="container">
+          <div className="mt-10 w-full grid mb-20 gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {donations.map((x, i) => (
+              <div
+                key={i}
+                className="overflow-hidden bg-white rounded-xl shadow"
+              >
+                <div className="relative">
+                  <div className="absolute top-3 right-3 py-2 px-3 text-sm rounded-lg bg-[var(--greencal-primary)] text-white">
+                    {x.category}
                   </div>
-                  <div className="flex items-center gap-2">
-                    <Icon name="bx-map" color="#B6E4CE" size="15px" />
-                    <span>{x.location}</span>
+                  <div className="absolute bottom-0 left-3 py-1 px-3 text-sm rounded-tl-lg rounded-tr-lg bg-[var(--greencal-main)] text-white">
+                    {x.date}
                   </div>
+                  <Image
+                    alt="Greencal upcoming event event"
+                    src={x.image}
+                    width={300}
+                    height={200}
+                    className="object-cover w-full h-auto"
+                  />
                 </div>
 
-                <p className="mb-3 font-bold text-left text-xl text-gray-700">
-                  {x.name}
-                </p>
+                <div className="p-4">
+                  <div className="mb-2 flex gap-4 text-xs">
+                    <div className="flex items-center gap-2">
+                      <Icon name="bx-time" color="#B6E4CE" size="15px" />
+                      <span>{x.time}</span>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <Icon name="bx-map" color="#B6E4CE" size="15px" />
+                      <span>{x.location}</span>
+                    </div>
+                  </div>
 
-                <button className="flex items-center text-lg text-[#49BA82] gap-2">
-                  Read More
-                  <Icon name="bx-chevron-right" color="#49BA82" size="30px" />
-                </button>
-
-                <div className="relative my-4 w-full h-3 bg-[var(--greencal-main)] rounded-full">
-                  <div className="absolute top-0 left-0 h-full w-[65%] bg-[var(--greencal-primary)] rounded-full"></div>
-                </div>
-                <div className="flex items-center justify-between">
-                  <p className="text-sm text-gray-700">
-                    <b className="text-black">₦ {x.raised.toLocaleString()}</b>{" "}
-                    of ₦ {x.goal.toLocaleString()} goal
+                  <p className="mb-3 font-bold text-left text-xl text-gray-700">
+                    {x.name}
                   </p>
-                  <Link href={"/donations/" + x.slug}>
-                    <Icon name="bx-donate-heart" size="25px" />
-                  </Link>
+
+                  <button className="flex items-center text-lg text-[#49BA82] gap-2">
+                    Read More
+                    <Icon name="bx-chevron-right" color="#49BA82" size="30px" />
+                  </button>
+
+                  <div className="relative my-4 w-full h-3 bg-[var(--greencal-main)] rounded-full">
+                    <div className="absolute top-0 left-0 h-full w-[65%] bg-[var(--greencal-primary)] rounded-full"></div>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm text-gray-700">
+                      <b className="text-black">
+                        ₦ {x.raised.toLocaleString()}
+                      </b>{" "}
+                      of ₦ {x.goal.toLocaleString()} goal
+                    </p>
+                    <Link href={"/donations/" + x.slug}>
+                      <Icon name="bx-donate-heart" size="25px" />
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
       {events && events.length > 0 && (
