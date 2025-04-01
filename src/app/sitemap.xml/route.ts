efc5b019cead28cc4db9db00a28ff5cf5ade0841
Moses Nwigberi <moses.nwigberi@ditcosoft.com>
2025-04-01 21:19:57 +0100

    updated sitemap

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -2,26 +2,26 @@ import { getDocuments, getDonations } from "@/helpers/lib/firebase";
 import { DonationsType, IPost } from "@/types";
 import { NextResponse } from "next/server";
 
-const siteUrl = "https://yourwebsite.com"; // Replace with your actual domain
+const siteUrl = "https://greencalfoundation.com";
 
 export async function GET() {
   // Fetch dynamic pages (e.g., blog posts, products, etc.)
-  const donations = await getDonations() as DonationsType[]
-  const events = await getDocuments() as IPost[]
+  const donations = (await getDonations()) as DonationsType[];
+  const events = (await getDocuments()) as IPost[];
 
   // Define static routes
-  const staticPages = ["", "/about", "/contact","/events","/donations"];
+  const staticPages = ["", "/about", "/contact", "/events", "/donations"];
 
   // Generate XML structure
   const urls = [
     ...staticPages.map((page) => `<url><loc>${siteUrl}${page}</loc></url>`),
     ...donations.map(
       (post: { slug: string }) =>
-        `<url><loc>${siteUrl}/blog/${post.slug}</loc></url>`
+        `<url><loc>${siteUrl}/donations/${post.slug}</loc></url>`
     ),
     ...events.map(
       (post: { slug: string }) =>
-        `<url><loc>${siteUrl}/blog/${post.slug}</loc></url>`
+        `<url><loc>${siteUrl}/events/${post.slug}</loc></url>`
     ),
   ];
 
